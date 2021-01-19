d4f2cd2cbb9e31bacbdca4e245c1c67a9a5d5ac7
Sankalp mishra <sankalp1902@gmail.com>
2021-01-19 15:12:37 +0530

    Login success made working

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   get "super", to:"super#index"
   get "reception", to:"reception#index"
   get "Authenticate", to:"authenticate#index"
+  get "authenticate", to:"authenticate#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
