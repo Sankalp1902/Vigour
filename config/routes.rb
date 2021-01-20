5bb69ebe8df4a35b876f14b9fb0313280fcef1e6
Sankalp mishra <sankalp1902@gmail.com>
2021-01-20 18:48:32 +0530

    Routes working properly, turbolinks removed.

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do  
  
-  get 'authenticate/index'
-  get '', to: "welcome#index"
-  get "welcome", to: "welcome#index"
-  get "contact", to:"contact#index"
-  get "admin", to:"admin#index"
-  get "super", to:"super#index"
-  get "reception", to:"reception#index"
-  get "Authenticate", to:"authenticate#index"
-  get "authenticate", to:"authenticate#index"
-  get "forgot-password", to:"authenticate#forgot-password"
+  root "welcome#index"   
+  get '' => "welcome#index"
+  get "welcome" => "welcome#index"
+  get "contact" => "contact#index"
+  get "admin" => "admin#index"
+  get "super" => "super#index"
+  get "reception" => "reception#index"
+  get "Authenticate" => "authenticate#index"
+  get "authenticate" => "authenticate#index"
+  get "forgot-password" => "authenticate#forgot-password"
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
