d961d0b46339249c487d5f684005f56c8f29dbfa
Sankalp mishra <sankalp1902@gmail.com>
2021-01-21 15:34:54 +0530

    Unauthorized access prevented

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,9 @@ Rails.application.routes.draw do
   get "admin" => "admin#index"
   get "super" => "super#index"
   get "reception" => "reception#index"
-  get "Authenticate" => "authenticate#index"
   get "authenticate" => "authenticate#index"
   get "forgot-password" => "authenticate#forgot-password"
   get "logout" => "authenticate#logout"
-  
+  get "loading" => "authenticate#loading"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
